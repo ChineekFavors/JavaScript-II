8daf13b55ee3f491a94c7be5505467e35a79abce
Chineek Favors <chineekfavors01@gmail.com>
2019-05-25 00:31:48 -0700

    finish all challenges in array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,7 +57,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
 runners.forEach(function(element){
-	return fullName.push([element['first_name'],  element['last_name']]);
+	fullName.push([element['first_name'],  element['last_name']]);
 });
 
 console.log(fullName);
@@ -68,23 +68,52 @@ console.log(fullName);
 let allCaps = runners.map(function(x){
 	return x['first_name'].toUpperCase();
 })
-console.log(allCaps); 
+//console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+let largeShirts = runners.filter(function(x){
+	return x['shirt_size'] === 'L';
+})
+//console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(a,b){
+		return a += b.donation;
+}, 0)
+
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//function find runner with the first name falito
+let findFalito = runners.filter(function(element){
+	return element['first_name'] === 'Falito';
+})
+//console.log(findFalito);
 
 // Problem 2
+// function add 10 more dollars to runners donation
+let addTen = runners.map(function(a){
+	return a['donation'] + 10;
+})
+//console.log(addTen);
+
 
 // Problem 3
+//this function will get runners first name and email address 
+let runnersEmail = [];
+runners.forEach(function(x){
+		runnersEmail.push([x.first_name, x.email]);
+});
+//console.log(runnersEmail);
+
+
+
+
+
+
